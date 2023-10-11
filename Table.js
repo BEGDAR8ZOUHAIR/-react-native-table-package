1789c3db5ad51f489482d0e3680e206203ec14d5
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-10-11 22:29:19 +0100

    add table

---
+++ Table.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { DataTable } from 'react-native-paper';
+
+const CustomTable = ({ columns, data }) =>
+{
+    return (
+        <DataTable>
+            <DataTable.Header>
+                {columns.map((column, index) => (
+                    <DataTable.Title key={index}>{column}</DataTable.Title>
+                ))}
+            </DataTable.Header>
+
+            {data.map((row, rowIndex) => (
+                <DataTable.Row key={rowIndex}>
+                    {row.map((cell, cellIndex) => (
+                        <DataTable.Cell key={cellIndex}>{cell}</DataTable.Cell>
+                    ))}
+                </DataTable.Row>
+            ))}
+        </DataTable>
+    );
+};
+
+export default CustomTable;
